1e73b5ecb22a75e6108fbfc32c64cedaaf889ac2
kshitij kumar <kshitijk83@gmail.com>
2019-03-06 19:36:52 +0530

    events done

--- js/index.js
+++ js/index.js
@@ -1,21 +1,49 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var clubs = document.querySelectorAll('.clubs-con-img');
+    
+    clubs.forEach(club=>{
+        club.addEventListener('click', function(){
+            popupBack.style.opacity = 1;
+            popupBack.style.visibility = 'visible';
+        })
+    })
+
+    var close = document.querySelector('.popup__close');
+    var popupBack = document.querySelector('.popup');
+    console.log(close);
+    close.addEventListener('click', function(){
+        popupBack.style.opacity = 0;
+        popupBack.style.visibility = 'hidden';
+    })
+});
+
+
 // $('[has-ripple="true"]').click(function () {
-// // $(this).toggleClass('clicked');
+// $(this).toggleClass('clicked');
 // $('.menu').toggleClass('open');
 // });
 
+// $(document).ready()
+
 // $('.menu a').each(function (index) {
-// var thismenuItem        = $(this);
+// var thismenuItem= $(this);
 
 // thismenuItem.click(function (event) {
 //     event.preventDefault();
+//     console.log('hey');
+    // $('.menuitem-wrapper').eq(index).addClass('spin');
     
-//     $('.menuitem-wrapper').eq(index).addClass('spin');
-    
-//     var timer = setTimeout(function () {
-//     $('body').removeAttr('class').addClass('bg-'+index);
-//     $('.menuitem-wrapper').eq(index).removeClass('spin');
-//     $('.menu').removeClass('open');
-//     $('.menu-btn').removeClass('clicked');
-//     }, 800);
+    // var timer = setTimeout(function () {
+    // $('body').removeAttr('class').addClass('bg-'+index);
+    // $('.menuitem-wrapper').eq(index).removeClass('spin');
+    // $('.menu').removeClass('open');
+    // $('.menu-btn').removeClass('clicked');
+    // }, 800);
 // });
 // });
+// $('navigation_icon').click(function(){
+//     console.log('hey');
+// })
